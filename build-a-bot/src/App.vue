60d56cffc52dfbe5f8003948a924c169131fb3ce
rnara <rajakumarsnara@outlook.com>
2018-09-05 14:35:38 +0000

    v

--- build-a-bot/src/App.vue
+++ build-a-bot/src/App.vue
@@ -1,5 +1,20 @@
 <template>
   <div id="app">
+<button v-on:click="loadPosts">OData</button>    
+    <ul>
+    <li v-for="post in posts.value">
+    {{post}}
+    </li>
+    </ul>
+    <hr/>
+    <ul>
+    <li v-for="(v,k,i) in posts">
+    {{i}}: {{k}} - {{v}}
+    </li>
+    </ul>
+<hr/>
+ 
+
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -7,12 +22,27 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
-
+import appService from './app.service.js';
 export default {
   name: 'app',
   components: {
     HelloWorld,
   },
+  data(){
+    return{
+      posts:[]
+    }
+  },
+  methods:{
+    loadPosts:function(){
+      
+      appService.getPosts().then(data =>{
+        this.posts=data
+      })
+      
+      alert(posts);
+    }
+  }
 };
 </script>
 
